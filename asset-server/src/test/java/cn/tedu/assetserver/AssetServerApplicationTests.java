dc8e6d8cf8b380bc26c0b2ef14a248912205c149
kmaiasuka <kamiasuka@asuka.top>
2023-12-26 21:05:05 +0800

    前后端

---
+++ asset-server/src/test/java/cn/tedu/assetserver/AssetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.assetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
