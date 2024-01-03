002a2521897924d63d205cd5d2de4dcf26570b14
volador <volador@ted.cn>
2024-01-03 17:21:18 +0800

    更新了路由

--- asset-web/src/router/index.js
+++ asset-web/src/router/index.js
@@ -10,7 +10,10 @@ const routes = [
     children:[
       {path:'/index', component: () => import( '../views/home/IndexView')},
       {path:'/dept', component: () => import( '../views/home/DeptView')},
-      {path:'/user', component: () => import( '../views/home/UserView')}
+      {path:'/user', component: () => import( '../views/home/UserView')},
+      {path:'/asset-category', component: () => import( '../views/home/AssetCategoryView')},
+      {path:'/attachment', component: () => import( '../views/home/AttachmentView')},
+      {path:'/asset-manage', component: () => import( '../views/home/AssetManageView')},
     ]
   },
   {path:'/login', component: () => import( '../views/home/LoginView')}
