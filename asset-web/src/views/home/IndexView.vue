755d54b01c6e100a062cfddc8ef608c7b14bf916
volador <volador@ted.cn>
2024-01-12 19:06:13 +0800

    更新首页

--- asset-web/src/views/home/IndexView.vue
+++ asset-web/src/views/home/IndexView.vue
@@ -119,6 +119,7 @@ const showBarChart = () => {
 }
 /*柱状图展示部分--结束*/
 
+
 /*饼状图展示部分--开始*/
 const assetPieChartRef = ref(null);
 //绘制饼状图
@@ -128,11 +129,11 @@ const showPieChart = () => {
     let option = {
         backgroundColor: "#fff", // 设置背景颜色为白色
         title: { // 标题配置
-            text: "Customized Pie", // 标题文本
+            text: "资产价值统计", // 标题文本
             left: "center", // 标题居中
             top: 20, // 上边距为20
             textStyle: {
-                color: "#f00" // 标题文字颜色为红色
+                color: "#2c343c" // 标题文字颜色为红色
             }
         },
         tooltip: { // 提示框显示
@@ -151,7 +152,7 @@ const showPieChart = () => {
             orient: "vertical", // 图例纵向排列
             x: "left", // 图例位于左侧
             textStyle: {
-                color: "#f00" // 图例文字颜色为红色
+                color: "#2c343c" // 图例文字颜色为红色
             },
             data: ["土地", "房屋", "构筑物", "通用设备", "专用设备", "车辆", "文物和陈列品", "家具用具", "图书档案", "动植物", "无形资产", "在建工程"], // 图例的数据，与数据项名一一对应
         },
@@ -159,58 +160,66 @@ const showPieChart = () => {
             {
                 name: "访问来源", // 系列名称
                 type: "pie", // 图表类型为饼图
-                radius: ["55%"], // 饼图半径
+                radius: ["0%", "60%"], // 饼图半径
                 center: ["50%", "50%"], // 饼图中心点位置
                 data: [ // 数据项列表
-                    { value: 335, name: "直接访问" }, // 数据项，value：数值，name：数据项名称
-                    { value: 310, name: "邮件营销" },
-                    { value: 274, name: "联盟广告" },
-                    { value: 235, name: "视频广告" },
-                    { value: 400, name: "搜索引擎" }
-                ].sort(function(a, b) { // 按照数值从小到大排序
+                    {value: 5, itemStyle: {color: '#588EBD'}, name: "土地"}, // 数据项，value：数值，name：数据项名称
+                    {value: 20, itemStyle: {color: '#E25A5A'}, name: "房屋"},
+                    {value: 36, itemStyle: {color: '#45B6B0'}, name: "构筑物"},
+                    {value: 10, itemStyle: {color: '#8775A9'}, name: "通用设备"},
+                    {value: 10, itemStyle: {color: '#4F5C65'}, name: "专用设备"},
+                    {value: 20, itemStyle: {color: '#13AAE3'}, name: "车辆"},
+                    {value: 5, itemStyle: {color: '#939EB0'}, name: "文物和陈列品"},
+                    {value: 20, itemStyle: {color: '#F29603'}, name: "家具用具"},
+                    {value: 36, itemStyle: {color: '#9BCC34'}, name: "图书档案"},
+                    {value: 10, itemStyle: {color: '#BC8F8C'}, name: "动植物"},
+                    {value: 10, itemStyle: {color: '#FDB35C'}, name: "无形资产"},
+                    {value: 20, itemStyle: {color: '#49C0BE'}, name: "在建工程"}
+                ].sort(function (a, b) { // 按照数值从小到大排序
                     return a.value - b.value;
                 }),
-                roseType: "radius", // 南丁格尔玫瑰图，通过半径展现数据的大小，也可以改为 "area"，表示通过面积展现数据的大小
+                roseType: "", // 南丁格尔玫瑰图，通过半径展现数据的大小，也可以改为 "area"，表示通过面积展现数据的大小
                 label: {
-                    color: "rgba(30, 30, 30, 0.3)" // 标签文字颜色为灰色，透明度为 0.3
+                    color: "rgba(10, 10, 10, 0.9)" // 标签文字颜色为灰色，透明度为 0.3
                 },
                 labelLine: {
                     lineStyle: {
-                        color: "rgba(30, 30, 30, 0.3)" // 标签线颜色为灰色，透明度为 0.3
+                        color: "rgba(10, 10, 10, 0.9)" // 标签线颜色为灰色，透明度为 0.3
                     },
                     smooth: 0.2, // 标签线的弯曲程度
-                    length: 10, // 第一段标签线的长度
-                    length2: 20 // 第二段标签线的长度
+                    length: 5, // 第一段标签线的长度
+                    length2: 10 // 第二段标签线的长度
                 },
                 itemStyle: {
-                    color: "#c23531", // 每个扇形部分的颜色
+                    color: "#0f0", // 每个扇形部分的颜色
                     shadowBlur: 200, // 阴影的大小
-                    shadowColor: "rgba(0, 0, 0, 0.5)" // 阴影的颜色
+                    shadowColor: "rgba(255,255,255, 0.9)" // 阴影的颜色
                 },
 
                 animationType: "scale", // 动画类型：缩放
                 animationEasing: "elasticOut", // 缩放动画的效果
-                animationDelay: function(idx) { // 动画延时
+                animationDelay: function (idx) { // 动画延时
                     return Math.random() * 200;
                 }
             }
         ]
     };
-    setInterval(function() {
-        var data0 = option.series[0].data.sort(function(a, b) {
-            return a.value - b.value;
-        });
-        // { value: 335, name: "直接访问" }
-        let name = data0.shift();
-        data0.push({
-            value: Math.round(Math.random() * 400 + 1),
-            name: name.name
-        });
-        option.series[0].data = data0.sort(function(a, b) {
-            return a.value - b.value;
-        });
-        pieChart.setOption(option);
-    }, 2000);
+    pieChart.setOption(option);
+    // setInterval(function() {
+    //     let data0 = option.series[0].data.sort(function(a, b) {
+    //         return a.value - b.value;
+    //     });
+    //     { value: 5, name: "土地" }
+    //     let name = data0.shift();
+    //     data0.push({
+    //         value: Math.round(Math.random() * 400 + 1),
+    //         name: name.name
+    //     });
+    //     option.series[0].data = data0.sort(function(a, b) {
+    //         return a.value - b.value;
+    //     });
+    //     pieChart.setOption(option);
+    // }, 2000);
 
 }
 /*饼状图展示部分--结束*/
@@ -234,9 +243,11 @@ onMounted(() => {
     margin: 0;
     font-size: 16px; /* 调整为您希望的字体大小 */
     line-height: 20px; /* 调整为您希望的行高 */
+    color: white;
 }
 
 .card-text {
     line-height: 20px; /* 调整为您希望的行高 */
+    color: white;
 }
 </style>
