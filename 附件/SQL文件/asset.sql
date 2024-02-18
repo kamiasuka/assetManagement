619e4edca18f5cea8f1ebc58ccd2ef4d36cf8a91
volador <12601414+ajun446@user.noreply.gitee.com>
2024-02-18 07:25:28 +0000

    update 附件/SQL文件/asset.sql. 修改了user表和asset-category表，user表新增字段nickname，asset-category表移除字段type 新增部分数据
    Signed-off-by: volador <12601414+ajun446@user.noreply.gitee.com>

--- 附件/SQL文件/asset.sql
+++ 附件/SQL文件/asset.sql
@@ -6,21 +6,24 @@ use asset;
 DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
-    username varchar(30) primary key COMMENT '用户名',
-    password varchar(30) not null COMMENT '密码',
+    id       bigint primary key auto_increment COMMENT '主键',
+    username varchar(30) not null unique COMMENT '用户名',
+    password varchar(30) not null COMMENT '密码' DEFAULT '2309',
+    nickname varchar(30) not null COMMENT '昵称' DEFAULT '操作员',
     identity varchar(30) not null COMMENT '身份',
-    tel      varchar(30) DEFAULT '' COMMENT '联系电话',
-    email    varchar(30) DEFAULT '' COMMENT '联系邮箱',
-    dept     varchar(30) DEFAULT '' COMMENT '所属部门',
-    unit     varchar(30) DEFAULT '' COMMENT '所属单位'
-) COMMENT '登录用户' CHARSET = utf8mb4;
+    tel      varchar(30)                         DEFAULT '' COMMENT '联系电话',
+    email    varchar(30)                         DEFAULT '' COMMENT '联系邮箱',
+    dept     varchar(30)                         DEFAULT '' COMMENT '所属部门',
+    unit     varchar(30)                         DEFAULT '' COMMENT '所属单位'
+) COMMENT '用户' CHARSET = utf8mb4;
 
 # 部门表
 DROP TABLE IF EXISTS dept;
 CREATE TABLE dept
 (
-    id   varchar(50) primary key COMMENT '部门编码',
-    name varchar(50) not null COMMENT '部门名称',
+    id   bigint primary key auto_increment COMMENT '主键',
+    code varchar(50) unique not null COMMENT '部门编码',
+    name varchar(50)        not null COMMENT '部门名称',
     tel  varchar(30) DEFAULT '' COMMENT '联系电话',
     unit varchar(30) DEFAULT '' COMMENT '所属单位'
 ) COMMENT '部门' CHARSET = utf8mb4;
@@ -32,8 +35,7 @@ CREATE TABLE asset_category
     id        int primary key auto_increment COMMENT '主键',
     name      varchar(50) not null COMMENT '类别名称',
     parent_id int unsigned DEFAULT 0 COMMENT '父组类别ID，如果无父级，则为0',
-    level     int unsigned DEFAULT 0 COMMENT '级别，最顶级为1，次级为2，以此类推',
-    type      varchar(50)  DEFAULT '' COMMENT '资产类型',
+    level     int unsigned DEFAULT 1 COMMENT '级别，最顶级为1，次级为2，以此类推',
     is_parent int unsigned DEFAULT 0 COMMENT '是否包含子级，1=父级，0=不是父级'
 ) COMMENT '资产分类' CHARSET = utf8mb4;
 
@@ -41,41 +43,18 @@ CREATE TABLE asset_category
 DROP TABLE IF EXISTS asset_info;
 CREATE TABLE asset_info
 (
-    id            varchar(30) primary key COMMENT '资产编码',
-    name          varchar(50) not null COMMENT '资产名',
-    type          varchar(50) not null COMMENT '资产类型',
+    id            bigint primary key auto_increment COMMENT '主键',
+    code          varchar(30) unique not null COMMENT '资产编码',
+    name          varchar(50)        not null COMMENT '资产名',
+    type          varchar(50)        not null COMMENT '资产类型',
     dept          varchar(30)    DEFAULT '' COMMENT '所属部门',
     unit          varchar(30)    DEFAULT '' COMMENT '所属单位',
     life          int unsigned   DEFAULT 0 COMMENT '使用年限',
     amount        decimal(10, 2) DEFAULT 0.00 COMMENT '资产价值',
     use_status    varchar(30)    DEFAULT '闲置' COMMENT '使用状态，包括在用，闲置，在修，报废',
     review_status varchar(30)    DEFAULT '审核中' COMMENT '审核状态，包括审核中，已通过',
-    approval_date datetime       DEFAULT NULL COMMENT '审核通过日期',
-    note          text COMMENT '备注'
+    approval_date datetime       DEFAULT NULL COMMENT '审核通过日期'
 ) COMMENT '资产信息' CHARSET = utf8mb4;
-
-# 附件分类表
-DROP TABLE IF EXISTS attachment_category;
-CREATE TABLE attachment_category
-(
-    id          varchar(30) primary key COMMENT '批次号',
-    type        varchar(50) not null COMMENT '名称',
-    name        varchar(50) not null COMMENT '类型',
-    number      int      DEFAULT 0 COMMENT '附件数量',
-    update_time datetime DEFAULT NULL COMMENT '更新时间',
-    note        text COMMENT '备注'
-) COMMENT '附件分类' CHARSET = utf8mb4;
-
-# 附件信息表
-DROP TABLE IF EXISTS attachment_info;
-CREATE TABLE attachment_info
-(
-    id          varchar(30) primary key COMMENT '编号',
-    name        varchar(50)  not null COMMENT '名称',
-    file_url    varchar(100) not null COMMENT '文件路径',
-    update_time datetime DEFAULT NULL COMMENT '更新时间',
-    cat_id      varchar(30) COMMENT '批次号'
-) COMMENT '附件信息' CHARSET = utf8mb4;
 
 # 修改记录表
 DROP TABLE IF EXISTS modify_record;
@@ -90,12 +69,43 @@ CREATE TABLE modify_record
     past_time   datetime     not null COMMENT '审核通过时间'
 ) COMMENT '修改记录' CHARSET = utf8mb4;
 
+
 # 用户表操作
-INSERT INTO asset.user(username, password, identity, tel, email, dept, unit)
-VALUES ('admin', 'admin', '管理员', '12345678901', '1232@aa.com', '资产部', '宝相公司'),
-       ('auditor', 'auditor', '审核员', '11111111555', '4542@aa.com', '资产部', '宝相公司');
+INSERT INTO asset.user(username, password, nickname, identity, tel, email, dept, unit)
+VALUES ('admin', DEFAULT, 'admin', '管理员', '12345678901', '1232@aa.com', '资产部', '宝相公司'),
+       ('admin2', DEFAULT, DEFAULT, '管理员', '12345678901', '1232@aa.com', '资产部', '宝相公司'),
+       ('auditor', DEFAULT, 'auditor', '审核员', '11111111555', '4542@aa.com', '资产部', '宝相公司');
+# INSERT INTO asset.user(username, password, identity, tel, email, dept, unit) VALUES ('admin','sc','管理员2','121','123ss2@aa.com','资产部','宝相公司');
 
 # 部门表操作
-INSERT INTO asset.dept(id, name, tel, unit)
-VALUES ('000001', '资产部', '', ''),
-       ('000002', '测试部', '', '');
+INSERT INTO asset.dept(code, name, tel, unit)
+VALUES ('1', '资产部', '12345678900', '宝相公司'),
+       ('2', '测试部门', '12345678901', '宝相公司'),
+       ('3', '办公室', '12345678910', '宝相公司'),
+       ('4', '财务部', '12345678911', '宝相公司');
+
+# 资产分类表操作
+INSERT INTO asset.asset_category(name, parent_id, level, is_parent)
+VALUES ('土地', DEFAULT, DEFAULT, DEFAULT),
+       ('房屋', DEFAULT, DEFAULT, DEFAULT),
+       ('构筑物', DEFAULT, DEFAULT, DEFAULT),
+       ('通用设备', DEFAULT, DEFAULT, DEFAULT),
+       ('专用设备', DEFAULT, DEFAULT, DEFAULT),
+       ('车辆', DEFAULT, DEFAULT, DEFAULT),
+       ('文物和陈列品', DEFAULT, DEFAULT, DEFAULT),
+       ('家具用具', DEFAULT, DEFAULT, DEFAULT),
+       ('图书档案', DEFAULT, DEFAULT, DEFAULT),
+       ('动植物', DEFAULT, DEFAULT, DEFAULT),
+       ('无形资产', DEFAULT, DEFAULT, DEFAULT),
+       ('在建工程', DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO asset.asset_category(name, parent_id, level, is_parent)
+VALUES ('电子设备', 8, 2, 0);
+UPDATE asset.asset_category
+set is_parent=1
+where id = 8;
+
+# 资产信息表操作
+INSERT INTO asset.asset_info(code, name, type, dept, unit, life, amount, use_status, review_status, approval_date)
+VALUES ('1', '土地测试', '土地', '', '宝相公司', 50, 1000000.00, DEFAULT, DEFAULT, DEFAULT),
+       ('2', '电脑a', '电子设备', '', '宝相公司', 5, 3000.00, DEFAULT, DEFAULT, DEFAULT),
+       ('3', '办公桌a', '家具用具', '', '宝相公司', 20, 10000.00, DEFAULT, DEFAULT, DEFAULT);
