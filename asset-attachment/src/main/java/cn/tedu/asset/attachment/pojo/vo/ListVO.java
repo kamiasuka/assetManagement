53e1fd8046083bbddf3c025bad86f6f617e45171
何浩川 <13510075+he-haochuan43@user.noreply.gitee.com>
2024-03-02 10:23:00 +0800

    分页查询

--- asset-attachment/src/main/java/cn/tedu/asset/attachment/pojo/vo/ListVO.java
+++ asset-attachment/src/main/java/cn/tedu/asset/attachment/pojo/vo/ListVO.java
@@ -1,6 +1,9 @@
 package cn.tedu.asset.attachment.pojo.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class ListVO {
@@ -8,4 +11,7 @@ public class ListVO {
     private String Type;
     private String Name;
     private String Status;
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private String updatedTime;
+
 }
