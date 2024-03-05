9ccb21a5165ba4f11cfd7a4478186e4108c1edfc
volador <volador@ted.cn>
2024-03-05 21:08:22 +0800

    更新dept

---
+++ asset-manage/src/main/java/cn/tedu/asset/manage/service/IDeptService.java
@@ -0,0 +1,18 @@
+package cn.tedu.asset.manage.service;
+
+import cn.tedu.asset.manage.pojo.dto.DeptAddDTO;
+import cn.tedu.asset.manage.pojo.dto.DeptUpdateDTO;
+import cn.tedu.asset.manage.pojo.vo.DeptVO;
+
+import java.util.List;
+
+public interface IDeptService {
+    List<DeptVO> listAll();
+
+    int addNew(DeptAddDTO deptAddDTO);
+
+    int deptUpdate(DeptUpdateDTO deptUpdateDTO);
+
+    int deptDelete(String code);
+
+}
--- asset-web/src/views/home/base/DeptView.vue
+++ asset-web/src/views/home/base/DeptView.vue
@@ -9,22 +9,28 @@
                         placeholder="XX公司"
                         class="input-with-select"
                 >
-                    <template #append>
-                        <el-button :icon="Search" />
-                    </template>
+                    <el-input style="width: 300px;">
+                        <template #append>
+                            <el-button :icon="Search"></el-button>
+                        </template>
+                    </el-input>
                 </el-input>
                 部门名称：
                 <el-input style="width: 180px" ></el-input>
                 <el-button @click="clearFilter" type="primary">查询</el-button>
             </div>
             <el-table :data="tableData" stripe style="width: 100%">
-                <el-table-column prop="date" label="部门名称" width="180"></el-table-column>
-                <el-table-column prop="name" label="所属单位" width="180"></el-table-column>
-                <el-table-column prop="address" label="部门编码"></el-table-column>
-                <el-table-column prop="address" label="部门电话"></el-table-column>
-                <el-table-column prop="address" label="操作">
-                    <span>修改</span>
-                    <span>删除</span>
+                <el-table-column prop="name" label="部门名称" width="180"></el-table-column>
+                <el-table-column prop="unit" label="所属单位" width="180"></el-table-column>
+                <el-table-column prop="code" label="部门编码"></el-table-column>
+                <el-table-column prop="tel" label="部门电话"></el-table-column>
+                <el-table-column fixed="right" label="操作">
+                    <template #default>
+                        <div v-if="user.identity==='管理员'">
+                            <el-button type="success" size="small">编辑</el-button>
+                            <el-button type="danger" size="small">删除</el-button>
+                        </div>
+                    </template>
                 </el-table-column>
             </el-table>
         </el-tab-pane>
@@ -63,7 +69,40 @@
 </template>
 
 <script setup>
-    import {Search} from '@element-plus/icons-vue';
+import {
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
+    Check,
+    Delete,
+    Edit,
+    Message,
+    Search,
+    Star,
+} from '@element-plus/icons-vue'
+import {onMounted, reactive, ref} from 'vue'
+import axios from "axios";
+import {ElMessage} from "element-plus";
+    //获取用户登录信息（session）
+    const user = ref(localStorage.user?JSON.parse(localStorage.user):null);
+
+const tableData = ref([{id:"",code:"",unit:"",tel:""}]);
+
+// const tableData = ref([{name:"cc",code:"a",unit:"ab",tel:"111"}]);
+
+onMounted(()=>{
+   axios.get("http://localhost:9002/v1/dept/listAll")
+       .then((response)=>{
+           if (response.data.code==2001){
+               ElMessage.success("操作成功");
+               tableData.value = response.data.data;
+               console.log(tableData);
+               console.log(response.data.data);
+           }
+       })
+});
+
 </script>
 
 <style scoped>
