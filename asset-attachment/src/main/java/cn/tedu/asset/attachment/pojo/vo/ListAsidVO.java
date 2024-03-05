45871c0aa6a5d54f922e17e186c949ce15eff2a1
何浩川 <13510075+he-haochuan43@user.noreply.gitee.com>
2024-03-05 11:51:38 +0800

    关联表3

--- asset-attachment/src/main/java/cn/tedu/asset/attachment/pojo/vo/ListAsidVO.java
+++ asset-attachment/src/main/java/cn/tedu/asset/attachment/pojo/vo/ListAsidVO.java
@@ -3,16 +3,14 @@ package cn.tedu.asset.attachment.pojo.vo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
-public class ListVO {
-    private String unit;
+public class ListAsidVO {
+    private String assetId;
     private String Type;
     private String atName;
     private String name;
+    private String Status;
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private String updatedTime;
-    private String Status;
 
 }
