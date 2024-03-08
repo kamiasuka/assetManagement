e3002f5127ebe69658d5b2a4182bd951d2350cf9
volador <volador@ted.cn>
2024-03-08 20:55:29 +0800

    附件sql更新

--- 附件/SQL文件/attachment.sql
+++ 附件/SQL文件/attachment.sql
@@ -1,9 +1,10 @@
 use asset;
+DROP TABLE IF EXISTS AttachmentList;
 CREATE TABLE AttachmentList (
     id INT PRIMARY KEY auto_increment,
     assetId VARCHAR(255),
     Type VARCHAR(255),
-    Name VARCHAR(255),
+    atName VARCHAR(255),
     Tip  VARCHAR(255),
     Status VARCHAR(255),
     url    varchar(255),
